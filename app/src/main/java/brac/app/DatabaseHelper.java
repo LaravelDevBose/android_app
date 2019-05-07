9e2eefb619f21241dbccb7488d28154e4d19d5c8
King Fisher <tokingfisher19@gmail.com>
2019-05-07 15:58:58 +0600

    Inital commit

---
+++ app/src/main/java/brac/app/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package brac.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "students";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
+
+    public DatabaseHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
